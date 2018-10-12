716d08ea47bd977fc23189c058e0f48bf24e919b
prashant.gangwar <prashant.gangwar@sofocle.com>
2018-10-12 16:40:55 +0530

    UI fixes and flash msg for wrong creds

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,52 +1,55 @@
 var express = require('express');
 var router = express.Router();
-
 var app = express();
-
 var mongoose = require('mongoose');
 var url ='mongodb://localhost:27017/polling-app';
 mongoose.connect(url);
 var questions = require("./src/questions.js");
-
 var registrationSchema = require('./src/registration').collection;
-var loginSchema = require('./src/login')
+//var loginSchema = require('./src/login')
 var bodyParser = require('body-parser');//pull post content from http request
-
-var urlencodedParser = bodyParser.urlencoded({extended:true});
-var bodyParser = require('body-parser');//pull post content from http request
-
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 app.use(express.static('./views'));
 app.use(bodyParser());
 
-
-var cookieParser = require('cookie-parser')
-app.use(cookieParser())
-
 var session = require('express-session');
-app.use(session({secret: 'prashant'}));
+var cookieParser = require('cookie-parser')
+var flash1 = require('connect-flash');
+var flash = require('express-flash');
+app.use(cookieParser('secret'))
+app.use(session({secret: 'prashant',saveUninitialized:true , resave:true,cookie: { maxAge: 60000 }}));
+router.use(flash());
+
+router.get('/flash', function(req, res){
+  // Set a flash message by passing the key, followed by the value, to req.flash().
+  req.flash('info', 'Flash is back!')
+  res.redirect('/');
+});
+
+//JWT
+var jwt = require('jsonwebtoken');
+app.set('secret', 'prashant');
+//JWT
 
 var sess;
-
-
 var id = count;
+var count = 1;
+var msg
 
 router.get('/', function (req, res) {
     res.send("hello");
 });
 
 router.post('/addOptions',async function (req, res) {
-  
    if(sess.voted==false){
    id =id;
    var VoteCount = 1;
    var option= req.body.textbox;
    var optionselected = req.body.optionselected;
     if(option==''){
-    
     console.log("in if");
     var resp =await questions.updateOne({option:optionselected},{$inc:{VoteCount:1}});
     console.log("mongo",resp)
-
   }
   else{
     console.log("in else")
@@ -55,7 +58,6 @@ router.post('/addOptions',async function (req, res) {
     console.log(resp);
     count++;
   }
-
   await registrationSchema.updateOne({email:sess.email},{$set:{voted:true}});
  res.send("voted");
 }
@@ -64,45 +66,22 @@ else{
 }
 });
 
-
-
 router.get('/login',function(req,res){
 	res.render('login.ejs');
 });
 
 router.get('/dashboard',async function(req,res,result){
-
   try{
     var voted = await registrationSchema.findOne({email:sess.email});
     sess.voted =voted.voted; 
-    console.log("voted arr", voted.voted);
-    console.log("hhhhhhhhhhhhhhhhhhhh",sess.email);
-
-      let resp = await questions.find({});
-      console.log(resp)
-            // if (err) return console.log(err)
-      res.render('dashboard.ejs',{data:resp,voted:voted.voted});
+    sess.name = voted.name;
+    let resp = await questions.find({});
+    res.render('dashboard.ejs',{data:resp,voted:voted.voted,name:voted.name});
   }
   catch(e){
       console.log(e)
   }
-
 });
-// router.post('/dashboard',async function(req,res,result){
-
-//   try{
-    
-//     var voted = await registrationSchema.findOne({email:sess.email}); 
-//     if(voted.voted==false){
-//       var id =req.body.id;
-//       var resp= await questionSchema.updateOne({id:4},{$inc:{VoteCount:1}})
-//     }
-//   }
-//   catch(e){
-//       console.log(e)
-//   }  
-// });
-
 
 router.post('/login', urlencodedParser , async function(req,res,next){
   if (!req.body) return res.sendStatus(400)
@@ -112,20 +91,35 @@ var query = {
       email:req.body.email,
       password:req.body.password
 };
-
   try{
-  
-      let resp = await registrationSchema.findOne(query);
-      //console.log(resp);
-      if (resp.length == 0) res.send("Not found");
+    let resp  = await registrationSchema.findOne(query);
+    if(!resp)
+    {
+      req.flash('success', ', Enter Valid Exixting Email ID');   //   req.flash('success', 'Registration successfully');
+      res.locals.message = req.flash();
+       res.render('login');
+      //res.send("not foud");
+    }
+    else{
+      console.log("login",resp);
+      if (resp.length == 0  ) {
+        req.flash('success', ', Enter Valid Exixting Email ID');   //   req.flash('success', 'Registration successfully');
+      res.locals.message = req.flash();
+       res.render('login');
+      }
       else{
+        console.log("isActive",resp.active);
+        if(resp.active==true){
         res.redirect('/api/dashboard');
+        }
+        else{
+          req.flash('success', ', Verify Your Mail First');   //   req.flash('success', 'Registration successfully');
+          res.locals.message = req.flash();
+           res.render('login');        }
       }
     }
-  catch(e){
-      console.log(e)
   }
-
+  catch(e){console.log(e)}
 });
 
 // mailer
@@ -139,39 +133,33 @@ auth: {
 });
 //mailer
 
-//JWT
-var jwt = require('jsonwebtoken');
-app.set('secret', 'prashant');
-//JWT
-var count = 1;
-
-
-
-
-var msg
 // verify mail 
-router.get('verify', (req,res)=>{
+router.get('/verify', (req,res)=>{
   console.log("in verify")
   let token=req.query.token
   jwt.verify(token, app.get('secret'), async function(err, decoded) {
-    // add the decoded user name and org name to the request object
-    // for the downstream code to use
           username = decoded.username;
           var resp= await registrationSchema.updateOne({email:username},{ $set:{ "active": true } })
-          console.log(username);
-    //logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
+          console.log("username of verified mail",username);
+          res.send("verifiedff");
 });
 })
 // verify mail
 
 router.post('/registration', urlencodedParser, async function (req, res) {
       if (!req.body) return res.sendStatus(400)
+      var query = {
+        email:req.body.email,
+      };
+      let resp  = await registrationSchema.findOne(query);
+     // console.log('response for checking emial; existance',resp);
+
+      if(!resp){
       req.body.id=count;
       var active=false;
       var voted=false;
       let a = {name,email,password} = req.body;// decorative syntax
       a={id,name,email,password,active,voted};
-      console.log(a);
       emailTo = req.body.email;
       // generate token for emaIL
       var token = jwt.sign({
@@ -179,29 +167,14 @@ router.post('/registration', urlencodedParser, async function (req, res) {
           username: email   // username is now set to the email specified
       }, app.get('secret'));
       // generate token for emaIL
-
-      // obj = {
-      //     id: 1,
-      //     name: 'Hello',
-      //     email: 'test',
-      //     password: 'test',
-      //      active :0
-      // }
       try{
-         // let resp = await (new registrationSchema(obj).save())  
-         // var registrationSchema = require('./api/routes/src/registration').collection; without .collection
-
          let resp = await registrationSchema.insertOne(a);
          count++;
-
-         // return res.send("inserted succesfully")
-         // console.log(res)
-
           // mailer
           var mailOptions = {
               from: 'prashant.dreamkix@gmail.com',
               to: emailTo,
-              subject: 'Sending Email using Node.js',
+              subject: 'verify your mail for voting app',
               text: 'http://localhost:8080/api/verify?token='+token
             };
             
@@ -214,14 +187,18 @@ router.post('/registration', urlencodedParser, async function (req, res) {
             });
           //mailer
           res.redirect('/');
-
       }catch(e){
           console.log(e)
       }
+
+    }
+
+    else{
+      req.flash('success', ', Email ID already exists');   //   req.flash('success', 'Registration successfully');
+      res.locals.message = req.flash();
+       res.render('registration');
+    }
   })
-
-
-
 // catch 404
 router.use((req, res, next) => { 
   res.status(404).send('<h2 align=center>Page Not Found!</h2>');
--- app.js
+++ app.js
@@ -2,20 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');//pull post content from http request
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //var mongo = require('mongo');
-var abc = require('./api/routes/index.js');
 var router = express.Router();
-var urlencodedParser = bodyParser.urlencoded({extended:true});
 //If extended is false, you can not post "nested object"
 // Nested Object = { person: { name: cw } } needed in app for options
 var app = express();
 app.use(express.static('./views'));
-app.use(bodyParser());
+//app.use(bodyParser());//depreciated
+app.use(bodyParser.urlencoded({
+	extended: true
+  }));
+app.use(bodyParser.json());
 //app.use('/api', router);
 app.set('view engine','ejs');
 
+//
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use(require('express-jquery')('/jquery.js'));
+
+//
+
 //session
 var session = require('express-session');
-app.use(session({secret: 'prashant'}));
+app.use(session({secret: 'prashant',saveUninitialized: true , resave:true}));
 
 //sssion
 
@@ -33,7 +41,7 @@ app.get('/registration',function(req,res){
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/polling-app');*/
 
-app.listen(8080, () => {
-    console.log("listining on port 8080");
+app.listen(8081, () => {
+    console.log("listining on port 8081");
 })
 
